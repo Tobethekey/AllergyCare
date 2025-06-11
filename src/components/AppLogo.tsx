e5a7c1d25a9d8e5610052ec309f46df2992f833e
Schulemail Key <keyschool2024@gmail.com>
2025-06-11 09:53:22 +0000

    Das hat nicht die filigrane Liniendesign. Versuche es erneut. Mache die

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -13,14 +13,15 @@ const AppLogo: React.FC = () => {
       >
         {/* Background circle */}
         <circle cx="12" cy="12" r="11.5" fill="#809580"/>
-        
-        {/* New Symbol Path (stylized drop/leaf with intertwined loop) */}
-        {/* This path creates the main shape and a hole for the smaller loop's interior */}
-        <path 
+
+        {/* Filigree Line Symbol Path */}
+        <path
           fillRule="evenodd"
           clipRule="evenodd"
           d="M12.0001 5.5C10.5501 6.04002 8.56005 7.98997 7.50005 11.4C6.98005 13.34 7.40006 16.32 9.50006 17.57C9.30006 18.01 9.50006 18.91 12.0001 19.5C14.5001 18.91 14.7001 18.01 14.5001 17.57C16.6001 16.32 17.0201 13.34 16.5001 11.4C15.4401 7.98997 13.4501 6.04002 12.0001 5.5ZM9.60005 13.3C9.20005 13.11 8.85005 13.36 8.81005 13.78C8.71005 14.92 9.49005 15.86 10.4201 15.98C11.3001 16.09 12.0701 15.42 12.1301 14.6C12.2101 13.57 11.1901 12.86 10.2301 13.06C10.0001 13.11 9.81005 13.2 9.60005 13.3Z"
-          fill="#fffaef"
+          stroke="#fffaef"
+          fill="none"
+          strokeWidth="0.8" // Adjusted for finer lines
         />
       </svg>
     </div>
