90e1a1784adbfe76ace57aaf5bfb8b351b39f5a0
Schulemail Key <keyschool2024@gmail.com>
2025-06-11 09:51:49 +0000

    Nun möchte ich abschließend noch einmal das Logo angehen. Ich finde die

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -11,47 +11,17 @@ const AppLogo: React.FC = () => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <defs>
-          {/* Defined a more rounded, fuller petal shape */}
-          <path id="petal" d="M0 -8 Q-2.5 0 0 1.5 Q2.5 0 0 -8Z" fill="#fffaef"/>
-        </defs>
-        {/* Outer circle: primary color */}
+        {/* Background circle */}
         <circle cx="12" cy="12" r="11.5" fill="#809580"/>
-
-        {/* Outer Petals - Scaled up for more presence */}
-        <use href="#petal" transform="translate(12 12) rotate(0) scale(1.4)"/>
-        <use href="#petal" transform="translate(12 12) rotate(30) scale(1.4)"/>
-        <use href="#petal" transform="translate(12 12) rotate(60) scale(1.4)"/>
-        <use href="#petal" transform="translate(12 12) rotate(90) scale(1.4)"/>
-        <use href="#petal" transform="translate(12 12) rotate(120) scale(1.4)"/>
-        <use href="#petal" transform="translate(12 12) rotate(150) scale(1.4)"/>
-        <use href="#petal" transform="translate(12 12) rotate(180) scale(1.4)"/>
-        <use href="#petal" transform="translate(12 12) rotate(210) scale(1.4)"/>
-        <use href="#petal" transform="translate(12 12) rotate(240) scale(1.4)"/>
-        <use href="#petal" transform="translate(12 12) rotate(270) scale(1.4)"/>
-        <use href="#petal" transform="translate(12 12) rotate(300) scale(1.4)"/>
-        <use href="#petal" transform="translate(12 12) rotate(330) scale(1.4)"/>
-
-        {/* Inner Petals - Slightly smaller scale */}
-        <use href="#petal" transform="translate(12 12) rotate(15) scale(1.0)"/>
-        <use href="#petal" transform="translate(12 12) rotate(45) scale(1.0)"/>
-        <use href="#petal" transform="translate(12 12) rotate(75) scale(1.0)"/>
-        <use href="#petal" transform="translate(12 12) rotate(105) scale(1.0)"/>
-        <use href="#petal" transform="translate(12 12) rotate(135) scale(1.0)"/>
-        <use href="#petal" transform="translate(12 12) rotate(165) scale(1.0)"/>
-        <use href="#petal" transform="translate(12 12) rotate(195) scale(1.0)"/>
-        <use href="#petal" transform="translate(12 12) rotate(225) scale(1.0)"/>
-        <use href="#petal" transform="translate(12 12) rotate(255) scale(1.0)"/>
-        <use href="#petal" transform="translate(12 12) rotate(285) scale(1.0)"/>
-        <use href="#petal" transform="translate(12 12) rotate(315) scale(1.0)"/>
-        <use href="#petal" transform="translate(12 12) rotate(345) scale(1.0)"/>
-
-        {/* Center Circle for Plus background */}
-        <circle cx="12" cy="12" r="3" fill="#fffaef" />
-
-        {/* Plus Sign: green (#809580), slightly thicker and with rounded corners */}
-        <rect x="10.75" y="9" width="2.5" height="6" fill="#809580" rx="0.5"/>
-        <rect x="9" y="10.75" width="6" height="2.5" fill="#809580" rx="0.5"/>
+        
+        {/* New Symbol Path (stylized drop/leaf with intertwined loop) */}
+        {/* This path creates the main shape and a hole for the smaller loop's interior */}
+        <path 
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M12.0001 5.5C10.5501 6.04002 8.56005 7.98997 7.50005 11.4C6.98005 13.34 7.40006 16.32 9.50006 17.57C9.30006 18.01 9.50006 18.91 12.0001 19.5C14.5001 18.91 14.7001 18.01 14.5001 17.57C16.6001 16.32 17.0201 13.34 16.5001 11.4C15.4401 7.98997 13.4501 6.04002 12.0001 5.5ZM9.60005 13.3C9.20005 13.11 8.85005 13.36 8.81005 13.78C8.71005 14.92 9.49005 15.86 10.4201 15.98C11.3001 16.09 12.0701 15.42 12.1301 14.6C12.2101 13.57 11.1901 12.86 10.2301 13.06C10.0001 13.11 9.81005 13.2 9.60005 13.3Z"
+          fill="#fffaef"
+        />
       </svg>
     </div>
   );
